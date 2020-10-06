7df0b68a4e6d5f65ec526f9b323394248ee2ac05
slefort <slefort@macbook-pro-de-slefort.home>
2020-10-06 19:34:16 +0200

    bootstrap Chapter 5

---
+++ Docs/Chapter5.md
@@ -0,0 +1,1 @@
+# Chapter 5 - Saving user Input: Testing the Database
---
+++ chapter05/superlists/lists/tests.py
@@ -0,0 +1,13 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        # Validate that Django uses the home template
+        self.assertTemplateUsed(response, 'home.html')
