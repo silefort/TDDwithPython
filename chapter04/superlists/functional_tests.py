31c3b42f8f4c623462ece5fab5f7804a7e3deb40
slefort <slefort@macbook-pro-de-slefort.home>
2020-10-06 19:30:23 +0200

    Add a form to the page

--- chapter04/superlists/functional_tests.py
+++ chapter04/superlists/functional_tests.py
@@ -20,7 +20,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -29,7 +28,6 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
         
-
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
@@ -40,12 +38,14 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.asserTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
