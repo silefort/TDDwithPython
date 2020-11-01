32c708ea828eb82d228d2de7e0856cd07c61a774
Simon Lefort <simon@lefort.com>
2020-11-01 12:08:24 +0100

    Custom passwordless auth backend + custom user model

--- chapter19/superlists/accounts/views.py
+++ chapter19/superlists/accounts/views.py
@@ -1,5 +1,38 @@
+from accounts.models import Token
+from django.contrib import auth
+from django.contrib import messages
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
+import sys
 
 def send_login_email(request):
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+    send_mail(
+        'Your login link for Superlists',
+        message_body,
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
     return redirect('/')
+
+def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    return redirect('/')
+    # print('login_view', file=sys.stderr)
+    # uid = request.GET.get('uid')
+    # user = auth.authenticate(uid=uid)
+    # if user is not None:
+        # auth.login(request, user)
+    # return redirect('/')
--- chapter19/superlists/superlists/settings.py
+++ chapter19/superlists/superlists/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
